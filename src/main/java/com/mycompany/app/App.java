78a4a82a27d3d50987168dc2dc30e2b026f7fdf6
gorakh <gorak4u@gmail.com>
2018-12-22 21:42:33 +0530

    Modified App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,12 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+ 	Scanner scanner = new Scanner(System.in);
+	System.out.println("Enter 1st number");
+	int a = scanner.nextInt();
+	System.out.println("Enter 2st number");
+	int b = scanner.nextInt();
+	System.out.println("Sum is " + a+b);
     }
 
     private final String getMessage() {
